97a6d709eb1bdf4e88bd97ad5071dbe0a753c254
eyalgromzin <eyalgromzin@gmail.com>
2023-11-28 14:12:29 +0200

    show govs using redux

--- src/db/government.ts
+++ src/db/government.ts
@@ -89,4 +89,41 @@ exports.createGovernmentMethods = (app, db) => {
             });
         }
     });
+
+    app.get("/gov/getAllGovernments", (req, res) => {
+        try {
+            const sql = `select * from government`;
+            db.all(sql, [], (err, rows) => {
+                if (err) {
+                    return res.json({
+                        status: 300,
+                        success: false,
+                        error: err,
+                    });
+                }
+
+                if (rows < 1) {
+                    return res.json({
+                        status: 300,
+                        success: false,
+                        error: "no rows matched",
+                    });
+                }
+
+                return res.json({
+                    status: 200,
+                    success: true,
+                    data: rows,
+                });
+            });
+        } catch (err) {
+            console.log("failed to get article", err);
+
+            return res.json({
+                status: 400,
+                success: false,
+                error: err,
+            });
+        }
+    });
 };
